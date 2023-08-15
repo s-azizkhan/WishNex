92a139c8e4fa87854f709510f5a1e35049fc72bd
Aziz Khan <aziz@logicwind.com>
2023-08-16 00:34:54 +0530

    release beta on WishNex

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReactionController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -17,26 +20,38 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-})->name('home');
+/**
+ * Run artisan cmd to setup on fresh install
+ */
+Route::get('/install', function () {
+    Artisan::call('migrate:fresh');
+    Artisan::call('optimize');
+    return redirect()->route('home');
+});
+Route::get('/cc', function () {
+    Artisan::call('optimize');
+    return redirect()->route('home');
+});
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/wish-list', [HomeController::class, 'wishList'])->name('public.wish.list');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // reactions route
+    Route::post('add-reaction/{post}', [ReactionController::class, 'add_reaction'])->name('posts.reactions.store');
+
     // post routes
     Route::resource('posts', PostController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
